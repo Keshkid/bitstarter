ee7a6f696f70d44b695e305b2bfeecdc328f9461
Keshkid <keshkid@yahoo.co.uk>
2013-07-29 21:38:54 +0000

    Changed web.js to read index.html for display string

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+// AF:29.07.2013
+var fs = require('fs');
+var str = fs.readFileSync('index.html').toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World! 2');
+  //response.send('Hello World! 2');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
